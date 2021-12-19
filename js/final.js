a3475c87b796de5cec00c11fcfc3bc41109ff0ac
Bluelining <bluethelining@gmail.com>
2021-12-19 20:48:03 +0900

    2nd upload

--- js/final.js
+++ js/final.js
@@ -1,42 +1,66 @@
 var audios = [
-    new Audio('music/Tending_The_Garden.mp3'),
-    new Audio('music/Tending_The_Garden.mp3'),
+    new Audio('music/Its_Been_A_Year.mp3'),
+    new Audio('music/Rolling_Down_This_Lazy_Wave.mp3'),
     new Audio('music/Tending_The_Garden.mp3'),
     new Audio('music/No_More_Fire.mp3')
 ]
+var titles = ["It's Been A Year - No Spirit X Kyle McEvoy", "Rolling Down This Lazy Wave - santpoort", "Tending The Garden - Stan Forebee", "No More Fire - Sleepy Fish"];
 var volumes = [0,0,0,0];
 var flags = [0,0,0,0];
+var music_flag = false;
 const MAX_INDEX = 4;
 
+audios[0].loop = true;
+audios[1].loop = true;
+audios[2].loop = true;
+audios[3].loop = true;
+
+var title = document.getElementById('song');
+var button = document.getElementById('button');
 var currentPlaying = null;
 window.addEventListener('scroll',ScrollMusic);
 
+
+
 function ScrollMusic()
 {
-    if(window.scrollY >= 100 & window.scrollY < 200)
+    if(window.scrollY >= 0 & window.scrollY < 3200)
+    {
+        exclusivePlay(0);
+    }
+    else if(window.scrollY >= 3200 & window.scrollY < 6400)
+    {
+        exclusivePlay(1);
+    }
+    else if(window.scrollY >= 6400 & window.scrollY < 9600)
     {
         exclusivePlay(2);
     }
-    if(window.scrollY >= 200 & window.scrollY < 300)
+    else if(window.scrollY >= 9600 & window.scrollY < 12800)
     {
         exclusivePlay(3);
     }
 }
 
+
 //해당 인덱스만 재생, 이외 중지
 function exclusivePlay(index)
 {
-    
     for(let i = 0; i < MAX_INDEX; i++)
     {
         if(i == index)
         {
             if(flags[i] != 1) //해당 인덱스
             {
-                console.log(i);
                 flags[i] = 1;
-                audios[i].play();
-                AccumulateVolume(i,1);
+                if(music_flag)
+                {
+                    audios[i].play();
+                    title.innerText = "Now Playing : " + titles[i];
+                    AccumulateVolume(i,1);
+                }
+
+                document.getElementById("season"+(i+1)).style.visibility = "visible";
             }           
         }
         else
@@ -44,7 +68,11 @@ function exclusivePlay(index)
             if(flags[i] != -1) //이외
             {
                 flags[i] = -1;
-                AccumulateVolume(i,-1);
+                if(music_flag)
+                {
+                    AccumulateVolume(i,-1);
+                }
+                document.getElementById("season"+(i+1)).style.visibility = "hidden";
             }      
         }
     }
@@ -81,29 +109,66 @@ function AccumulateVolume(index,direction)
     setTimeout(AccumulateVolume,20,index,direction);
 }
 
-
-function castParallax() {
+//패럴랙스 계산
+function parallax() {
 
 	window.addEventListener("scroll", function(event){
-
-        console.log(window.scrollY);
-		var top = this.scrollY;
+        //console.log(window.scrollY);
 		var layers = document.getElementsByClassName("parallax");
-		var layer, speed, yPos;
+		var layer, speed, y;
 		for (var i = 0; i < layers.length; i++) {
 			layer = layers[i];
-			speed = layer.getAttribute('data-speed');
-			var yPos = -(top * speed / 100);
-			layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
-
+            if(layer.parentElement.style.visibility != 'hidden')
+            {
+                var topY = this.scrollY - layer.getAttribute('data-offset');
+                speed = layer.getAttribute('data-speed');
+                var y = -(topY * speed / 100);
+                layer.setAttribute('style', 'transform: translate3d(0px, ' + y + 'px, 0px)');
+    
+            }
 		}
 	});
+}
 
+function onLoad()
+{
+    parallax();
+
+    //스크롤 책갈피 리스너
+
+    document.getElementById('bm1').addEventListener('click',function(){window.scrollTo(0,0);});
+    document.getElementById('bm2').addEventListener('click',function(){window.scrollTo(0,3200);});
+    document.getElementById('bm3').addEventListener('click',function(){window.scrollTo(0,6400);});
+    document.getElementById('bm4').addEventListener('click',function(){window.scrollTo(0,9600);});
+
+    button.addEventListener('click',function(){
+        if(music_flag)
+        {
+            music_flag = false;
+            for(var i = 0 ; i < MAX_INDEX; i ++)
+            {
+                button.innerText="[음악 켜기]";
+                title.innerText = "Now Playing : Muted";
+                audios[i].pause();
+                flags[i] = 0;
+            }
+        }
+        else
+        {
+            music_flag = true; 
+            for(var i = 0 ; i < MAX_INDEX; i ++)
+            {
+                button.innerText="[음악 끄기]";
+                flags[i] = 0;
+            }
+            ScrollMusic();
+        }
+
+
+    });
 
 }
 
 
+document.body.onload = onLoad();
 
-
-
-castParallax();
